ea54ae29c4bdeb67abb2c40b8051fc2524124f6d
ClementVaugoyeau <77982870+ClementVaugoyeau@users.noreply.github.com>
2022-02-16 12:07:15 +0100

    NavBAr et App js update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './_Components/NavBar';
+import { useNavigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <NavBar />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/_Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React, { Component, useState } from 'react'
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+
+export default function NavBar() {
+
+    
+    
+
+    return (
+         <div className='container'>
+             <div className='NavbarBody'>
+             <h2>Navbar</h2>
+
+             </div>
+         </div>
+        ) 
+}
